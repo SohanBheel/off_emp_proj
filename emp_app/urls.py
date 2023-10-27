8014be0f94dccfa7f2cf963b7bcde2be73a5419d
Sohan Bheel <mm8239239249@gmail.com>
2023-10-27 15:09:12 +0530

    Initial commit

---
+++ emp_app/urls.py
@@ -0,0 +1,32 @@
+"""office_emp_proj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include # add
+from . import views
+from django.views.generic.base import TemplateView # add
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('remove_emp/<int:emp_id>', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
+    path('account/', include('django.contrib.auth.urls')), # add
+    path('', TemplateView.as_view(template_name='home.html'), name = 'home'),
+    path('social-auth/', include('social_django.urls', namespace='social'))   
+]
+
